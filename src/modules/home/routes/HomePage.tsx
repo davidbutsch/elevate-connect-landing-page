a51b6ef62057fb56f4b210311938802473922fdf
Kaito Nishikawa <kai.nishikawa808@gmail.com>
2025-06-18 20:15:49 -0700

    added expansion for cards

--- src/modules/home/routes/HomePage.tsx
+++ src/modules/home/routes/HomePage.tsx
@@ -1,10 +1,13 @@
 import { Hero } from "@/modules/home";
 import { Stack } from "@mui/material";
 
+
 export const HomePage = () => {
   return (
+    <>
     <Stack direction="column" gap={15}>
       <Hero />
     </Stack>
+    </>
   );
 };
